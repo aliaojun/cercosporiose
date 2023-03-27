349f520f05f702e8fae0190c9c0e0a6a59c9d835
aliaojun <56044740+aliaojun@users.noreply.github.com>
2023-03-27 10:24:58 +0200

    Update st_demo.py

--- st_demo.py
+++ st_demo.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file,sep=";")
 elif st.button('Load example data'):
     df = pd.read_csv('output/data_ajout_nb_traitement.csv',sep = ';')
-    #df = df.sample(frac=0.01, random_state=RANDOM_STATE).reset_index(drop=True)
+    df = df.sample(frac=0.01, random_state=RANDOM_STATE).reset_index(drop=True)
     
 if df is not None:
     st.write('Here is the raw data')
